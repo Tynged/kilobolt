1aa519cc8204424374eaa1c6e625b8faf10b0af9
Tynged <tynged@gmail.com>
2013-09-08 23:07:50 -0500

    Day 6 completed: added enemies.

---
+++ KiloboltTutorial/src/gamepkg/Heliboy.java
@@ -0,0 +1,11 @@
+package gamepkg;
+
+
+public class Heliboy extends Enemy {
+
+	public Heliboy(int centerX, int centerY) {
+		setCenterX(centerX);
+		setCenterY(centerY);
+	}
+
+}
--- KiloboltTutorial/src/gamepkg/StartingClass.java
+++ KiloboltTutorial/src/gamepkg/StartingClass.java
@@ -13,7 +13,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	private static final long serialVersionUID = -2160611519642595667L;
 	private Robot robot;
-	private Image image, currentSprite, character, characterDown, characterJumped, background;
+	private Image image, currentSprite, character, characterDown, characterJumped, background, heliboy;
+	private Heliboy hb, hb2;
 	private URL base;
 	private Graphics second;
 	
@@ -51,6 +52,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 				currentSprite = character;
 			}
 			
+			hb.update();
+			hb2.update();
 			bg1.update();
 			bg2.update();
 			repaint();
@@ -91,6 +94,9 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
 		g.drawImage(currentSprite, robot.getCenterX() - robotCenterX,
 				robot.getCenterY() - robotCenterY, this);
+		
+		g.drawImage(heliboy, hb.getCenterX() - 48, hb.getCenterY() - 48, this);
+		g.drawImage(heliboy, hb2.getCenterX() - 48, hb2.getCenterY() - 48, this);
 
 		super.paint(g);
 	}
@@ -114,6 +120,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		characterDown = getImage(base, "data/down.png");
 		characterJumped = getImage(base, "data/jumped.png");
 		currentSprite = character;
+		heliboy = getImage(base, "data/heliboy.png");
 		background = getImage(base, "data/background.png");
 
 		addKeyListener(this);
@@ -125,6 +132,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		bg1 = new Background(0,0);
 		bg2 = new Background(2160, 0);
 		robot = new Robot();
+		hb = new Heliboy(340, 360);
+		hb2 = new Heliboy(700, 360);
 
 		Thread thread = new Thread(this);
 		thread.start();
